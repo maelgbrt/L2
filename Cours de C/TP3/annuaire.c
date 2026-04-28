6226d24f56218c2c2167e06ef8b3081e096245b5
GekoPlay <97613551+GekoPlay@users.noreply.github.com>
2026-04-28 09:59:48 +0200

    maj

--- Cours de C/TP3/annuaire.c
+++ Cours de C/TP3/annuaire.c
@@ -416,24 +416,7 @@ void menu(Annuaire_hashmap *a)
 int main(int argc, char *argv[])
 {
 
-    Contact c = *create("mael", "07", "gaborit", "maelgaborit@gmail.com");
-    Contact c2 = *create("telio", "058", "gaborit", "teliogaborit@gmail.com");
-    Annuaire_hashmap a;
-    init(&a, 15);
-    add(&a, &c);
-    add(&a, &c2);
-    print(a);
-    Contact *c3 = findContact(&a, "telio", 'n');
-    if (c3 != NULL)
-    {
-        printContact(*c3);
-    }
-    // printf("========test extraction ======");
-    // extraire(&a,"nt");
-    menu(&a);
-
-    // findContact(a.tab[10],"mael",'n');
-    // Contact *res = findContact(&a[10],"mael","n");
+    
 }
 
 
